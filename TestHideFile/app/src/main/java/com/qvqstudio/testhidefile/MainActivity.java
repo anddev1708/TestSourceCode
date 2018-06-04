2813a8f98844bd7f1d957384f9eecf84a87a337b
Anddev1708 <quyetqv@gmail.com>
2018-06-04 11:15:56 +0700

    fix rename

--- TestHideFile/app/src/main/java/com/qvqstudio/testhidefile/MainActivity.java
+++ TestHideFile/app/src/main/java/com/qvqstudio/testhidefile/MainActivity.java
@@ -3,7 +3,10 @@ package com.qvqstudio.testhidefile;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -157,6 +160,21 @@ public class MainActivity extends AppCompatActivity {
             return StorageState.READ_ONLY;
         }
 
+        return result;
+    }
+
+    /* Test move function in android */
+    private String getRealPathFromURI(Uri contentURI) {
+        String result;
+        Cursor cursor = getContentResolver().query(contentURI, null, null, null, null);
+        if (cursor == null) { // Source is Dropbox or other similar local file path
+            result = contentURI.getPath();
+        } else {
+            cursor.moveToFirst();
+            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            result = cursor.getString(idx);
+            cursor.close();
+        }
         return result;
     }
 }
